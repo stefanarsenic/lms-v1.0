b5794c0b5de4c245b1025f29594826c0e17c8ba1
Nikola <samo.za.igre876@gmail.com>
2024-03-06 16:41:00 +0100

    Dodao sam projektan

---
+++ server/TestGIT/src/main/java/rs/ac/singidunum/novisad/testgit/TestGitApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.novisad.testgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitApplication.class, args);
+    }
+
+}
