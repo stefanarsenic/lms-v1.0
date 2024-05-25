8b115609dd7dd905513eded284f7fab86bb81e72
Nikola <samo.za.igre876@gmail.com>
2024-05-25 15:08:16 +0200

    Dodat projekat

---
+++ server/src/test/java/rs/ac/singidunum/novisad/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.novisad.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
