dc2d0dbebf78762c53f6bf31d75a649d5dd45b55
Stefan Arsenic <stefan.arsenic@outlook.com>
2024-07-17 12:58:16 +0200

    greska

--- server/src/main/java/rs/ac/singidunum/novisad/server/ServerApplication.java
+++ server/src/main/java/rs/ac/singidunum/novisad/server/ServerApplication.java
@@ -1,11 +1,7 @@
 package rs.ac.singidunum.novisad.server;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import rs.ac.singidunum.novisad.server.model.korisnik.RegistrovaniKorisnik;
-import rs.ac.singidunum.novisad.server.security.AuthenticationService;
 
 @SpringBootApplication
 public class ServerApplication {
