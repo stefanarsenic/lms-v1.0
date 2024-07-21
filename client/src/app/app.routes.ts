fc89c7d920522f9acfb3f5ffa145fbbb1a8a13eb
Nikola <samo.za.igre876@gmail.com>
2024-07-21 12:01:11 +0200

    Admin component, fix ruta

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import {LoginComponent} from "./components/login/login.component";
 import {authGuard} from "./guards/auth.guard";
 import {UserProfileComponent} from "./components/user/user-profile/user-profile.component";
 import {RegisterComponent} from "./components/user/register/register.component";
+import {MainStudentComponent} from "./components/main-student/main-student.component";
 
 export const routes: Routes = [
   {
@@ -18,7 +19,7 @@ export const routes: Routes = [
       }
     ]
   },
-  {path: "main", component: MainComponent,canActivate:[authGuard],data:{
+  {path: "main", component: MainStudentComponent,canActivate:[authGuard],data:{
     allowedRoles:["ROLE_NASTAVNIK","ROLE_ADMIN"]
     }},
   {
