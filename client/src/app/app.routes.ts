37efb0495dc9f9c88b93b570c275a87aad448d2e
Nikola <samo.za.igre876@gmail.com>
2024-07-21 13:17:20 +0200

    Dizajn admina i ruta

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import {RegisterComponent} from "./components/user/register/register.component";
 import {MainStudentComponent} from "./components/main-student/main-student.component";
 import {FakultetComponent} from "./components/home-page/fakultet/fakultet.component";
 import {StudijskiProgramComponent} from "./components/home-page/studijski-program/studijski-program.component";
+import {AdminProfileComponent} from "./components/admin/admin-profile/admin-profile.component";
 
 export const routes: Routes = [
   {
@@ -35,6 +36,13 @@ export const routes: Routes = [
         component: StudijskiProgramComponent
       }
     ]
+  },
+  {
+    path:"admin",
+    component: AdminProfileComponent,
+    canActivate:[authGuard],data:{
+      allowedRoles:["ROLE_ADMIN"]
+    }
   },
   {path: "main", component: MainStudentComponent,canActivate:[authGuard],data:{
     allowedRoles:["ROLE_NASTAVNIK","ROLE_ADMIN"]
