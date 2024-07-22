808daca85bf4630c3c65025e5e095fa1498d5fa3
Nikola <samo.za.igre876@gmail.com>
2024-07-22 21:23:17 +0200

    Admin/Tabela korisnika

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,9 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 import {AuthInterceptor} from "./interceptors/auth.interceptor";
+import {provideAnimations} from "@angular/platform-browser/animations";
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),provideHttpClient(withInterceptorsFromDi()),
-    {provide:HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true}
+    {provide:HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true},
+    provideAnimations()
   ]
 };
--- client/src/app/components/admin/users/users.component.ts
+++ client/src/app/components/admin/users/users.component.ts
@@ -1,24 +1,58 @@
-import { Component } from '@angular/core';
+import {Component, Injector, ViewChild} from '@angular/core';
 import {GenerickaTabelaComponent} from "../../../../genericko/genericka-tabela/genericka-tabela.component";
 import {KorisnikService} from "../../../services/korisnik.service";
-
+import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {Table, TableModule} from 'primeng/table';
+import {ProgressBarModule} from "primeng/progressbar";
+import {TagModule} from "primeng/tag";
+import {NgClass} from "@angular/common";
+import {PaginatorModule} from "primeng/paginator";
+import {MultiSelectModule} from "primeng/multiselect";
+import {RegistrovaniKorisnik} from "../../../model/registrovaniKorisnik";
+import {SliderModule} from "primeng/slider";
+import {AppGenerickoComponent} from "../../../../genericko/app-genericko/app-genericko.component";
+import {IconFieldModule} from "primeng/iconfield";
+import {InputIconModule} from "primeng/inputicon";
+import {InputTextModule} from "primeng/inputtext";
+import {SortEvent} from "primeng/api";
 @Component({
   selector: 'app-users',
   standalone: true,
   imports: [
-    GenerickaTabelaComponent
+    GenerickaTabelaComponent,
+    ButtonModule,
+    TableModule,
+    ProgressBarModule,
+    TagModule,
+    NgClass,
+    PaginatorModule,
+    MultiSelectModule,
+    SliderModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule
   ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
-export class UsersComponent {
+export class UsersComponent extends AppGenerickoComponent<RegistrovaniKorisnik>{
 
-  constructor(private korisniciSerice:KorisnikService) {
-
+  loading: boolean = true;
+  @ViewChild('dt2') dt: Table | undefined;
+  constructor(private injector: Injector) {
+    super();
+    const service = this.injector.get(KorisnikService);
+    this.initialize(service);
+    this.loading=false
   }
 
-  korisnici:any=[]
+  ngOnInit() {
+  }
 
-
+  applyFilterGlobal($event: any, stringVal: any) {
+    this.dt!.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
+  }
 
 }
